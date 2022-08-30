e58a9b6382340ee64630020be3247cf4ce7f4b2b
big-by <sjmcruz@hotmail.com>
2022-08-30 20:03:30 -0300

    Adicionado modal de confirmação de exclusão

--- myfinance-web-netcore/Controllers/PlanoContaController.cs
+++ myfinance-web-netcore/Controllers/PlanoContaController.cs
@@ -53,11 +53,5 @@ namespace myfinance_web_netcore.Controllers
             return RedirectToAction("Index");
         }
 
-        public string OpenModelPopup()
-        {
-            //can send some data also.  
-            return "<h1>This is Modal Popup Window</h1>";
-        }
-
     }
 }
